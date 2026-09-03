7cf67bac6011c9a688ab708d8e9b021ef25c3ef6
Raju B <rajubailappa5456@gmail.com>
2026-09-03 16:52:13 +0530

    feat: add profile get endpoint

---
+++ backend/portfolio-backend/src/main/java/com/raju/portfolio/controller/ProfileController.java
@@ -0,0 +1,26 @@
+package com.raju.portfolio.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raju.portfolio.entity.Profile;
+import com.raju.portfolio.service.ProfileService;
+
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+
+    private final ProfileService profileService;
+
+    public ProfileController(ProfileService profileService) {
+        this.profileService = profileService;
+    }
+
+    @GetMapping
+    public List<Profile> getAllProfiles() {
+        return profileService.getAllProfiles();
+    }
+}
